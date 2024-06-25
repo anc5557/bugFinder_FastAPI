f4f86bea0d7cb60b79e7edbd7f20ffae313db8b6
junho <sjh01@crinity.com>
2024-06-25 13:34:07 +0900

    chore: Add .gitignore file and update Streamlit app

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI, HTTPException, Query
-from pymilvus import connections, FieldSchema, CollectionSchema, DataType, Collection, utility
-from pydantic import BaseModel
+from fastapi import FastAPI, File, HTTPException, Query, UploadFile
+from fastapi.encoders import jsonable_encoder
+from jsonschema import ValidationError
+import numpy as np
+from pymilvus import (
+    connections,
+    FieldSchema,
+    CollectionSchema,
+    DataType,
+    Collection,
+    utility,
+)
+from pydantic import BaseModel, ValidationError
 from typing import List
 from sentence_transformers import SentenceTransformer
 import logging
+import json
 
 app = FastAPI()
 
@@ -17,7 +28,7 @@ if not utility.has_collection(collection_name):
     fields = [
         FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),
         FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=768),
-        FieldSchema(name="metadata", dtype=DataType.VARCHAR, max_length=2048)
+        FieldSchema(name="metadata", dtype=DataType.VARCHAR, max_length=2048),
     ]
     schema = CollectionSchema(fields, "Bug reports collection")
 
@@ -40,7 +51,14 @@ def startup_event():
     try:
         if collection.is_empty:
             logger.info("Creating index for collection")
-            collection.create_index("embedding", index_params={"index_type": "IVF_FLAT", "metric_type": "L2", "params": {"nlist": 128}})
+            collection.create_index(
+                "embedding",
+                index_params={
+                    "index_type": "IVF_FLAT",
+                    "metric_type": "L2",
+                    "params": {"nlist": 128},
+                },
+            )
         collection.load()
     except Exception as e:
         logger.error(f"Error during startup: {e}")
@@ -58,12 +76,33 @@ class SearchQuery(BaseModel):
     query_text: str
 
 
+def convert_numpy_float_to_python_float(data):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            data[key] = convert_numpy_float_to_python_float(value)
+    elif isinstance(data, list):
+        data = [convert_numpy_float_to_python_float(item) for item in data]
+    elif isinstance(data, np.float32):
+        data = float(data)
+    return data
+
+
 @app.post("/add_bug/")
 def add_bug(report: BugReport):
     try:
-        metadata = f"작성자: {report.작성자} | 고객사: {report.고객사} | 업무: {', '.join(report.업무)} | 날짜: {report.날짜} | 버그 내용: {report.버그_내용}"
+        # 메타데이터를 JSON 형태로 변환
+        metadata = {
+            "작성자": report.작성자,
+            "고객사": report.고객사,
+            "업무": report.업무,
+            "날짜": report.날짜,
+            "버그_내용": report.버그_내용,
+        }
+        # JSON 객체를 문자열로 변환하여 저장
+        metadata_str = json.dumps(metadata, ensure_ascii=False)
         embedding = sentence_transformer_ef.encode([report.버그_내용])[0].tolist()
-        collection.insert([[embedding], [metadata]])
+        collection.insert([[embedding], [metadata_str]])
+        collection.flush()
         collection.load()
         return {"message": "Bug report added successfully"}
     except Exception as e:
@@ -77,47 +116,61 @@ def search_bug(query: SearchQuery):
         embedding = sentence_transformer_ef.encode([query.query_text])[0].tolist()
         search_params = {
             "metric_type": "L2",
-            "params": {"nprobe": 10}
+            "params": {"nprobe": 10},
         }
-        results = collection.search([embedding], "embedding", search_params, limit=3)
+        results = collection.search(
+            [embedding], "embedding", search_params, limit=3, output_fields=["metadata"]
+        )
+
         response = []
         for result in results:
             for res in result:
-                metadata = res.entity.get("metadata")
-                parsed_metadata = parse_metadata(metadata)
-                response.append({
-                    "작성자": parsed_metadata.get("작성자"),
-                    "고객사": parsed_metadata.get("고객사"),
-                    "업무": parsed_metadata.get("업무"),
-                    "버그_내용": parsed_metadata.get("버그 내용"),
-                    "거리": res.distance
-                })
+                logger.info(f"Result object: {res}")
+                metadata_str = res.entity.get("metadata")
+                if metadata_str is not None:
+                    metadata = json.loads(metadata_str)
+                    response.append(
+                        {
+                            "작성자": metadata.get("작성자"),
+                            "고객사": metadata.get("고객사"),
+                            "업무": metadata.get("업무"),
+                            "버그_내용": metadata.get("버그_내용"),
+                            "날짜": metadata.get("날짜"),
+                            "거리": res.distance,
+                        }
+                    )
+                else:
+                    logger.error(f"Metadata is None for result ID: {res.id}")
+
         return response
     except Exception as e:
         logger.error(f"Error searching bug report: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
 
-def parse_metadata(metadata):
-    """메타데이터 필드를 파싱하여 딕셔너리로 반환"""
-    fields = metadata.split(" | ")
-    parsed_data = {}
-    for field in fields:
-        key, value = field.split(": ", 1)
-        parsed_data[key] = value
-    return parsed_data
-
-
-@app.get("/bug_reports")
+@app.get("/bug_reports/")
 def bug_reports():
     try:
-        # 모든 엔트리 가져오기
-        bug_reports = collection.query(expr="id >= 0", output_fields=["id", "metadata"])
-        logger.info(f"Number of bug reports fetched: {len(bug_reports)}")
+        bug_report_data = collection.query(
+            expr="id > 0", output_fields=["id", "metadata"]
+        )
+        response = []
+        for report in bug_report_data:
+            metadata_str = report.get("metadata")
+            metadata = json.loads(metadata_str)
+            response.append(
+                {
+                    "id": report.get("id"),
+                    "작성자": metadata.get("작성자"),
+                    "고객사": metadata.get("고객사"),
+                    "업무": metadata.get("업무"),
+                    "버그_내용": metadata.get("버그_내용"),
+                    "날짜": metadata.get("날짜"),
+                }
+            )
 
-        return {
-            "bug_reports": bug_reports
-        }
+        logger.info(f"Number of bug reports fetched: {len(response)}")
+        return {"bug_reports": response}
     except Exception as e:
         logger.error(f"Error fetching bug reports: {e}")
         raise HTTPException(status_code=500, detail=str(e))
@@ -130,9 +183,69 @@ def delete_bug(report_id: int):
         if not result:
             raise HTTPException(status_code=404, detail="Bug report not found")
         collection.delete(expr=f"id == {report_id}")
+        collection.flush()
+        collection.load()
         return {"message": "Bug report deleted successfully"}
     except Exception as e:
         logger.error(f"Error deleting bug report: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/add_bug_reports_from_json/")
+async def add_bug_reports(reports: List[BugReport]):
+    try:
+
+        # 유효한 리포트를 Milvus에 추가
+        for report in reports:
+            metadata = {
+                "작성자": report.작성자,
+                "고객사": report.고객사,
+                "업무": report.업무,
+                "날짜": report.날짜,
+                "버그_내용": report.버그_내용,
+            }
+            metadata_str = json.dumps(metadata, ensure_ascii=False)
+            embedding = sentence_transformer_ef.encode([report.버그_내용])[0].tolist()
+            collection.insert([[embedding], [metadata_str]])
+
+        collection.flush()
+        collection.load()
+        return {
+            "message": "Bug reports added successfully",
+            "count": len(reports),
+        }
+    except ValidationError as e:
+        logger.error(f"Invalid report format: {e.json()}")
+        raise HTTPException(status_code=400, detail="Invalid report format")
+    except Exception as e:
+        logger.error(f"Error adding bug reports: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# 전체 컬렉션 삭제
+@app.delete("/delete_all/")
+def delete_all():
+    try:
+        collection.drop()
+        return {"message": "Collection deleted successfully"}
+    except Exception as e:
+        logger.error(f"Error deleting collection: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/get_bug/{report_id}")
+def get_bug(report_id: int):
+    try:
+        # 특정 id에 해당하는 문서 데이터를 조회
+        result = collection.query(expr=f"id == {report_id}", output_fields=["*"])
+        if not result:
+            raise HTTPException(status_code=404, detail="Bug report not found")
+        # numpy.float32를 Python float로 변환
+        result_converted = convert_numpy_float_to_python_float(result)
+        # 조회된 데이터를 반환
+        return jsonable_encoder({"data": result_converted})
+    except Exception as e:
+        logger.error(f"Error fetching bug report with id {report_id}: {e}")
         raise HTTPException(status_code=500, detail=str(e))
 
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,15 @@
 import streamlit as st
 import requests
+import json
 
 # Streamlit 애플리케이션 제목 및 설명
 st.title("버그 파인더")
 st.write("버그를 검색하거나 새로운 버그 리포트를 추가하세요.")
 
 # 탭 생성
-tab1, tab2, tab3 = st.tabs(["버그 검색", "버그 리포트 추가", "버그 리포트 목록"])
-
-
-def parse_metadata(metadata):
-    """메타데이터 필드를 파싱하여 딕셔너리로 반환"""
-    fields = metadata.split(" | ")
-    parsed_data = {}
-    for field in fields:
-        key, value = field.split(": ", 1)
-        parsed_data[key] = value
-    return parsed_data
+tab1, tab2, tab3, tab4 = st.tabs(
+    ["버그 검색", "버그 리포트 추가", "파일로 버그 리포트 추가", "버그 리포트 목록"]
+)
 
 
 def truncate_text(text, max_length=100):
@@ -27,24 +20,26 @@ def truncate_text(text, max_length=100):
 
 
 with tab1:
-    # 검색어 입력란
+    # 버그 검색 폼
     query_text = st.text_input("검색어를 입력하세요")
 
-    # 검색 버튼
     if st.button("검색"):
         if query_text:
-            # FastAPI 서버에 검색 요청
-            response = requests.post("http://localhost:8000/search_bug/", json={"query_text": query_text})
+            response = requests.post(
+                "http://localhost:8000/search_bug/", json={"query_text": query_text}
+            )
 
             if response.status_code == 200:
                 results = response.json()
-                st.write("검색 결과:")
-                for result in results:
+                st.write("가장 비슷한 버그 레포트 3개입니다.")
+                for id, result in enumerate(results, 1):
+                    st.write(f"{id}:")
                     st.write(f"작성자: {result['작성자']}")
                     st.write(f"고객사: {result['고객사']}")
+                    st.write(f"날짜: {result['날짜']}")
                     st.write(f"업무: {result['업무']}")
                     st.write(f"버그 내용: {result['버그_내용']}")
-                    st.write(f"거리: {result['거리']}")
+                    # st.write(f"거리: {result['거리']}")
                     st.write("---")
             else:
                 st.write("검색 중 오류가 발생했습니다.")
@@ -53,27 +48,28 @@ with tab1:
 
 with tab2:
     # 버그 리포트 추가 폼
-    with st.form(key='bug_report_form'):
+    with st.form(key="bug_report_form"):
         작성자 = st.text_input("작성자")
         고객사 = st.text_input("고객사")
         업무 = st.text_input("업무 (쉼표로 구분하여 입력)")
         버그_내용 = st.text_area("버그 내용")
         날짜 = st.date_input("날짜")
 
-        submit_button = st.form_submit_button(label='버그 리포트 추가')
+        submit_button = st.form_submit_button(label="버그 리포트 추가")
 
         if submit_button:
             if 작성자 and 고객사 and 업무 and 버그_내용 and 날짜:
-                # 입력된 데이터를 FastAPI 서버로 전송
                 업무_list = [x.strip() for x in 업무.split(",")]
                 report_data = {
                     "작성자": 작성자,
                     "고객사": 고객사,
                     "업무": 업무_list,
                     "버그_내용": 버그_내용,
-                    "날짜": str(날짜)
+                    "날짜": str(날짜),
                 }
-                response = requests.post("http://localhost:8000/add_bug/", json=report_data)
+                response = requests.post(
+                    "http://localhost:8000/add_bug/", json=report_data
+                )
                 if response.status_code == 200:
                     st.success("버그 리포트가 성공적으로 추가되었습니다.")
                 else:
@@ -82,29 +78,73 @@ with tab2:
                 st.error("모든 필드를 입력해주세요.")
 
 with tab3:
-    # FastAPI 서버에서 버그 리포트 가져오기
+    # 파일로 버그 리포트 추가
+    uploaded_file = st.file_uploader("버그 리포트 파일을 업로드하세요", type=["json"])
+    st.write(
+        "파일은 다음 필드를 포함해야 합니다: 작성자, 고객사, 업무, 버그_내용, 날짜"
+    )
+    st.write("날짜 형식: YYYY-MM-DD")
+    st.write("업무는 리스트 형태로 입력해주세요")
+
+    submit_button = st.button("버그 리포트 추가")
+
+    if submit_button and uploaded_file is not None:
+        try:
+            reports = json.load(uploaded_file)
+            if not isinstance(reports, list):
+                st.error("JSON 데이터는 리포트 목록이어야 합니다.")
+            else:
+                response = requests.post(
+                    "http://localhost:8000/add_bug_reports_from_json/",
+                    json=reports,
+                )
+                if response.status_code == 200:
+                    st.success("파일로부터 버그 리포트가 성공적으로 추가되었습니다.")
+                    st.session_state.file_uploader = None
+                else:
+                    st.error(
+                        f"파일로부터 버그 리포트 추가 중 오류가 발생했습니다: {response.json()}"
+                    )
+        except json.JSONDecodeError as e:
+            st.error(f"유효하지 않은 JSON 형식입니다: {e}")
+        except Exception as e:
+            st.error(f"파일 처리 중 오류가 발생했습니다: {e}")
+
+with tab4:
+    # 버그 리포트 목록
     response = requests.get(f"http://localhost:8000/bug_reports")
 
     if response.status_code == 200:
         data = response.json()
         bug_reports = data["bug_reports"]
 
-        # 자세히 보기 및 삭제 버튼 추가
-        for report in bug_reports:
-            parsed_metadata = parse_metadata(report["metadata"])
-            truncated_bug_content = truncate_text(parsed_metadata.get("버그 내용", ""))
+        if st.button("모든 버그 리포트 삭제"):
+            delete_all_response = requests.delete("http://localhost:8000/delete_all/")
+            if delete_all_response.status_code == 200:
+                st.success("모든 버그 리포트가 성공적으로 삭제되었습니다.")
+            else:
+                st.error("버그 리포트 삭제 중 오류가 발생했습니다.")
 
-            expander_text = f"{parsed_metadata.get('고객사')} | {parsed_metadata.get('업무')} | {truncated_bug_content}"
+        for report in bug_reports:
+            truncated_bug_content = truncate_text(report.get("버그_내용", ""))
+
+            tasks = ", ".join(report.get("업무", []))
+
+            expander_text = (
+                f"{report.get('고객사')} | {tasks} | {truncated_bug_content}"
+            )
             with st.expander(expander_text):
 
-                st.write(f"작성자: {parsed_metadata.get('작성자')}")
-                st.write(f"고객사: {parsed_metadata.get('고객사')}")
-                st.write(f"업무: {parsed_metadata.get('업무')}")
-                st.write(f"날짜: {parsed_metadata.get('날짜')}")
-                st.write(f"버그 내용: {parsed_metadata.get('버그 내용')}")
+                st.write(f"작성자: {report.get('작성자')}")
+                st.write(f"고객사: {report.get('고객사')}")
+                st.write(f"업무: {tasks}")
+                st.write(f"날짜: {report.get('날짜')}")
+                st.write(f"버그 내용: {report.get('버그_내용')}")
 
                 if st.button("삭제", key=f"delete_{report['id']}"):
-                    delete_response = requests.delete(f"http://localhost:8000/delete_bug/{report['id']}")
+                    delete_response = requests.delete(
+                        f"http://localhost:8000/delete_bug/{report['id']}"
+                    )
                     if delete_response.status_code == 200:
                         st.success("버그 리포트가 성공적으로 삭제되었습니다.")
                     else:
